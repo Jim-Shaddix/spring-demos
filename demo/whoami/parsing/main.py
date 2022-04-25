4d379ea8997363221e9e211167f3e66945327a7c
jamesshaddix <jimmy.shaddix2.0@gmail.com>
2022-04-24 19:07:15 -0700

    updated main

--- demo/whoami/parsing/main.py
+++ demo/whoami/parsing/main.py
@@ -2,16 +2,19 @@ import sys
 
 from lib.parser.ResponseCodeParser import ResponseCodeParser
 from lib.parser.HeaderParser import HeaderParser
+from lib.parser.RequestMethodParser import RequestMethodParser
 from lib.config.config import Config
 
 
 def main():
     header_website = sys.argv[0]
     response_code_website = sys.argv[1]
+    request_header_website = sys.argv[2]
     web_driver_path = sys.argv[2]
 
     HeaderParser.execute(Config.get_webdriver(header_website, web_driver_path))
     ResponseCodeParser.execute(Config.get_webdriver(response_code_website, web_driver_path))
+    RequestMethodParser.execute(Config.get_webdriver(request_header_website, web_driver_path))
 
 
 if __name__ == "__main__":
